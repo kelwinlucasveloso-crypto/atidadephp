fa3c00f16771bd226ddb836046e66185ab857914
kelwinlucasveloso-cripto <kelwinlucasveloso@gmail.com>
2026-07-01 09:30:50 -0300

    atividade

---
+++ operadores.php
@@ -0,0 +1,12 @@
+<?php
+$soma = 10+5;
+$subtracao = 10-5;
+$multiplicao = 10*5; 
+$divisao = 10/2;
+
+echo "Soma é $soma<br>";
+echo "Subtracão é $subtracao<br>";
+echo "Multiplicão é $multiplicao<br>";
+echo "Divisao é $divisao<br>";
+
+?>
